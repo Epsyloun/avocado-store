9a9a458b7590281da01ab8c6c1e5151420b938f6
Rodrigo Diaz <rodrigo.diaz8b@gmail.com>
2022-06-09 18:14:41 -0600

    primer commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,48 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+const imgUrl = "https://platzi-avo.vercel.app/";
+const app = document.querySelector('#app');
+
+const formatPrice = (price) =>{
+    const newPrice = new window.Intl.NumberFormat("en-EN",{
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+    return newPrice;
+}
+
+//Web api
+//Conectar al server
+    async function fetchData(){
+        const response = await fetch(url),
+        responseJson = await response.json(),
+        avocados = [];
+
+        responseJson.data.forEach(item => {
+            //crear imagen
+            const img = document.createElement('img');
+            img.src = imgUrl + item.image;
+
+            //crear titulo
+            const title = document.createElement('h2');
+            title.textContent = item.name;
+            title.className = "text-2xl text-red-600";
+
+            //crear precio
+            const price = document.createElement('div');
+            price.textContent = formatPrice(item.price);
+
+            //creando un contenedor para almacener los aguacates
+            const container = document.createElement('div');
+
+            container.append(img, title, price);
+
+            avocados.push(container);
+        });
+        app.append(...avocados)
+    }
+    fetchData();
+//Procesar la respuesta y convertirla en JSON
+//JSON -> DATA -> renderizar info en el browser
